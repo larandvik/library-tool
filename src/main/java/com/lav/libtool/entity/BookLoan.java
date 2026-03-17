663b1080542029d6fded5501caf108a556ed9c41
Larandvik <0658203@gmail.com>
2026-03-17 19:03:39 -0300

    REFACTORING BookLoan ADD @PrePersist for loanDate

--- src/main/java/com/lav/libtool/entity/BookLoan.java
+++ src/main/java/com/lav/libtool/entity/BookLoan.java
@@ -15,6 +15,7 @@ public class BookLoan {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // TODO: BookLoan хранит bookId/readerId как простые поля вместо связей JPA 
     @Column(nullable = false)
     private Long bookId;
 
@@ -22,7 +23,7 @@ public class BookLoan {
     private Long readerId;
 
     @Column(nullable = false)
-    private final LocalDate loanDate = LocalDate.now();
+    private LocalDate loanDate;
 
     @Column(nullable = false)
     private LocalDate dueDate;
@@ -32,6 +33,13 @@ public class BookLoan {
     @Column(nullable = false)
     private Status status = Status.ACTIVE;
 
+    @PrePersist
+    private void onCreate() {
+        if (loanDate == null) {
+            loanDate = LocalDate.now();
+        }
+    }
+
     public BookLoan(Long bookId, Long readerId, LocalDate dueDate) {
         this.bookId = bookId;
         this.readerId = readerId;
@@ -40,6 +48,10 @@ public class BookLoan {
 
     public void markReturned() {
         if (this.status == Status.RETURNED) {
+            // TODO: 1) У тебя есть кастомное исключение, но ты его не используешь, поэтому у тебя будет 500 вместо ожидаемого обработчика
+            //  2) Я бы сделал общую ошибку BookEXception или BookLoanException для всех типов искл этого домена с типом ошибки BOOK_ALREADY_RETURNED
+            //  3) и не надо харкодить текст ошибки, лучше использовать константы или enum для типов ошибок
+            // TODO:4) Тут такое же замечание как в Reader: я бы не стал делать проверку на статус в самом методе сущности, а просто позволил бы сервису контролировать логику возврата
             throw new IllegalStateException("Book already returned");
         }
 
